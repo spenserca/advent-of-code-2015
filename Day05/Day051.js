82ab6765e0fbc41e6a1ec411e488841692a11946
Spenser Andreassen <spenserca@gmail.com>
2018-11-17 17:17:01 -0600

    chore: linting

--- Day05/Day051.js
+++ Day05/Day051.js
@@ -1,36 +1,44 @@
-const invalidSequences = ['ab', 'cd', 'pq', 'xy'];
+const hasThreeVowels = (string) => {
+  let matches;
+
+  if (string.match(/[aeiou]/g)) {
+    matches = string.match(/[aeiou]/g);
+  }
+
+  return matches && matches.length >= 3;
+};
+
+const hasDoubleLetters = (string) => {
+  let doubleLetters = false;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i + 1]) {
+      doubleLetters = true;
+    }
+  }
+
+  return doubleLetters;
+};
+
+const containsInvalidSequence = (string) => {
+  let matches;
+
+  if (string.match(/ab|cd|pq|xy/g)) {
+    matches = string.match(/ab|cd|pq|xy/g);
+  }
+
+  return matches && matches.length > 0;
+};
 
 module.exports = (input) => {
   const naughtyNiceList = input.split(',');
   let niceStrings = 0;
 
-  naughtyNiceList.forEach(string => {
-    let isNice = false;
-
+  naughtyNiceList.forEach((string) => {
     if (hasThreeVowels(string) && hasDoubleLetters(string) && !containsInvalidSequence(string)) {
       niceStrings++;
     }
   });
 
   return niceStrings;
-}
-
-const hasThreeVowels = (string) => {
-  if (string.match(/[aeiou]/g)) {
-    return string.match(/[aeiou]/g).length >= 3
-  }
-}
-
-const hasDoubleLetters = (string) => {
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === string[i + 1]) {
-      return true;
-    }
-  }
-}
-
-const containsInvalidSequence = (string) => {
-  if (string.match(/ab|cd|pq|xy/g)) {
-    return string.match(/ab|cd|pq|xy/g).length > 0;
-  }
-}
+};
--- Day05/Day052.js
+++ Day05/Day052.js
@@ -1,34 +1,39 @@
-module.exports = (input) => {
-  const naughtyNiceList = input.split(',');
-  let niceStrings = 0;
-
-  naughtyNiceList.forEach(string => {
-    if (hasPalindrome(string) && hasMatchingPair(string)) {
-      niceStrings++;
-    }
-  });
-
-  return niceStrings;
-}
-
 const hasPalindrome = (string) => {
   for (let i = 0; i < string.length; i++) {
     if (string[i - 1] === string[i + 1]) {
       return true;
     }
   }
+
   return false;
-}
+};
 
 const hasMatchingPair = (string) => {
-  while (string.length > 2) {
-    let pairToMatch = string.substring(0, 2);
-    let stringToCompareTo = string.substring(2);
-    string = string.substring(1);
+  let toEvaluate = string;
+
+  while (toEvaluate.length > 2) {
+    const pairToMatch = toEvaluate.substring(0, 2);
+    const stringToCompareTo = toEvaluate.substring(2);
+
+    toEvaluate = toEvaluate.substring(1);
 
     if (stringToCompareTo.includes(pairToMatch)) {
       return true;
     }
   }
+
   return false;
-}
+};
+
+module.exports = (input) => {
+  const naughtyNiceList = input.split(',');
+  let niceStrings = 0;
+
+  naughtyNiceList.forEach((string) => {
+    if (hasPalindrome(string) && hasMatchingPair(string)) {
+      niceStrings++;
+    }
+  });
+
+  return niceStrings;
+};
